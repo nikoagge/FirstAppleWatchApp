4506ca412c59c190e595b39471176658e44a456d
nikoagge <nikoagge@gmail.com>
2023-10-14 17:57:08 +0300

    Initial Commit

---
+++ FirstAppleWatchApp/FirstAppleWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppleWatchAppApp.swift
+//  FirstAppleWatchApp
+//
+//  Created by Nikos Aggelidis on 14/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppleWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
